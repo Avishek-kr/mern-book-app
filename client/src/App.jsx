8ef8d4e2bae7003ccee90109e6576c8699435e11
Avishek-kr <ak251997@gmail.com>
2023-10-02 11:59:52 +0530

    added books, about, home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home/Home';
 import About from './Routes/About/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Book from './Routes/Book/Book';
 
 function App() {
   return (
     <>
       <Router>
-      Header
+      <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/books' element={<Book />} />
         </Routes>
-        Footer
+      <Footer />
       </Router>
     </>
   )
