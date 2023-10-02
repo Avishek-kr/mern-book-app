b4316f2f2c10f97319230874b1537850d98a82d0
Avishek-kr <ak251997@gmail.com>
2023-10-02 21:07:57 +0530

    added react fragmnet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Routes/Home/Home';
-import About from './Routes/About/About';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Book from './Routes/Book/Book';
-import Singlebook from './Routes/Book/Singlebook';
-import Createbook from "./Routes/Book/Createbook";
-import Editbook from "./Routes/Book/Editbook";
+import Home from './Routes/Home/Home.jsx';
+import About from './Routes/About/About.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Book from './Routes/Book/Book.jsx';
+import Singlebook from './Routes/Book/Singlebook.jsx';
+import Createbook from "./Routes/Book/Createbook.jsx";
+import Editbook from "./Routes/Book/Editbook.jsx";
 import React from 'react';
 
 
